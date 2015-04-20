b9f530981379b23c6b6a15bcc90f5228fe930d85
Alex Morega <alex@grep.ro>
2015-04-20 21:49:19 +0300

    rewrite as ecmascript 6

--- app.jsx
+++ app.jsx
@@ -1,15 +1,17 @@
 'use strict';
 
-$.get('https://api.github.com/repos/mgax/jekyllcms-testsite/git/trees/gh-pages?recursive=1', function(resp) {
-  console.log(
-    resp.tree.filter(function(i) {
+var repo = window.location.search.match(/[?&]repo=([^&\/]+\/[^&\/]+)\/?/)[1];
+var url = `https://api.github.com/repos/${repo}/git/trees/gh-pages?recursive=1`;
+$.get(url, (resp) => {
+  for(var r of resp.tree.filter((i) => {
       if(i.type != 'blob') return false;
       if(i.path == '.gitignore') return false;
       if(i.path == '_config.yml') return false;
       if(i.path.match(/^_layouts\//)) return false;
       return true;
-    })
-  );
+    })) {
+    console.log(r);
+  }
 });
 
 console.log(1);
