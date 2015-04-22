2352439ef60492b37a610d9f6830c181c2dd9989
Alex Morega <alex@grep.ro>
2015-04-22 18:53:27 +0300

    `api` method for api calls

--- github.jsx
+++ github.jsx
@@ -10,19 +10,15 @@ class GitHubFile {
 
   content() {
     if(! this.sha) { return Q(''); }
-    return Q($.ajax({
-      url: `${this.repo.api}/git/blobs/${this.sha}`,
-      headers: {Authorization: 'token ' + this.repo.token},
-    }))
-    .then((resp) =>
-      atob(resp.content));
+    return this.repo.api({url: '/git/blobs/' + this.sha})
+      .then((resp) =>
+        atob(resp.content));
   }
 
   save(newContent) {
-    return Q($.ajax({
-      url: `${this.repo.api}/contents/${this.path}`,
+    return this.repo.api({
+      url: '/contents/' + this.path,
       method: 'PUT',
-      headers: {Authorization: 'token ' + this.repo.token},
       data: JSON.stringify({
         branch: 'gh-pages',
         message: "Edit from JekyllCMS",
@@ -30,24 +26,23 @@ class GitHubFile {
         sha: this.sha,
         content: btoa(newContent)
       })
-    }))
+    })
     .then((resp) => {
       this.sha = resp.content.sha;
     });
   }
 
   delete() {
-    return Q($.ajax({
-      url: `${this.repo.api}/contents/${this.path}`,
+    return this.repo.api({
+      url: '/contents/' + this.path,
       method: 'DELETE',
-      headers: {Authorization: 'token ' + this.repo.token},
       data: JSON.stringify({
         branch: 'gh-pages',
         message: "Edit from JekyllCMS",
         path: this.path,
         sha: this.sha,
       })
-    }));
+    });
   }
 }
 
@@ -56,19 +51,20 @@ class GitHubRepo {
   constructor(repoName, token) {
     this.repoName = repoName;
     this.token = token;
-    this.api = `https://api.github.com/repos/${this.repoName}`;
+  }
+
+  api(options) {
+    options.url = 'https://api.github.com/repos/' + this.repoName + options.url;
+    options.headers = {Authorization: 'token ' + this.token}
+    return Q($.ajax(options));
   }
 
   files() {
-    var url = `${this.api}/git/trees/gh-pages?recursive=1`;
-    return Q($.ajax({
-      url: url,
-      headers: {Authorization: 'token ' + this.token},
-    }))
-    .then((resp) =>
-      resp.tree
-        .filter((i) => i.type == 'blob')
-        .map((i) => new GitHubFile(this, i)));
+    return this.api({url: '/git/trees/gh-pages?recursive=1'})
+      .then((resp) =>
+        resp.tree
+          .filter((i) => i.type == 'blob')
+          .map((i) => new GitHubFile(this, i)));
   }
 
   newFile(path) {
