b114206625e679a8752b0254db52819a5d2fc519
Alex Morega <alex@grep.ro>
2015-04-25 15:02:18 +0300

    extract ace to separate file

---
+++ ace.jsx
@@ -0,0 +1,23 @@
+'use strict';
+
+var Ace = React.createClass({
+  render: function() {
+    return <div ref="ace" className="aceContainer" />;
+  },
+  componentDidMount: function() {
+    this.reset(this.props.initial);
+  },
+  reset: function(content) {
+    if(this.ace) { this.ace.destroy(); }
+    var node = React.findDOMNode(this.refs.ace);
+    $(node).text(content);
+    this.ace = ace.edit(node);
+    this.ace.getSession().setMode('ace/mode/markdown');
+    this.ace.setTheme('ace/theme/tomorrow_night_eighties');
+    this.ace.setShowInvisibles(true);
+    this.ace.on('change', (e) => this.handleChange(e));
+  },
+  handleChange: function() {
+    this.props.onChange(this.ace.getValue());
+  }
+});
