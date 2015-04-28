87077f1ad805bda80ab591af446c065159d7b1df
Alex Morega <alex@grep.ro>
2015-04-28 09:42:25 +0300

    routing function

--- src/app.jsx
+++ src/app.jsx
@@ -2,36 +2,37 @@
 
 var app = {};
 
-$.get('config.json', (config) => {
-  app.errorBox = React.render(<ErrorBox />, document.querySelector('#errors'));
-
-  var topNode = document.querySelector('#top');
-  app.config = config;
+function route() {
   var authMatch = window.location.href.match(/\?code=(.*)/);
   if(authMatch) {
-    React.render(<AuthCallback code={authMatch[1]} />, topNode);
-    return;
+    return Q(<AuthCallback code={authMatch[1]} />);
   }
 
   app.authToken = localStorage.getItem('jekyllcms-github-token');
   if(! app.authToken) {
-    React.render(<AuthButton />, topNode);
-    return;
+    return Q(<AuthButton />);
   }
 
   app.gitHub = new GitHub(app.authToken);
 
   var repoMatch = window.location.search.match(/[?&]repo=([^&\/]+\/[^&\/]+)\/?/);
   if(repoMatch) {
-    app.gitHub.repo(repoMatch[1])
-      .then((repo) =>
-        React.render(<Site repo={repo} />, topNode))
+    return app.gitHub.repo(repoMatch[1])
+      .then((repo) => <Site repo={repo} />)
       .catch(errorHandler("loading repository"));
   }
   else {
-    app.gitHub.user()
-      .then((user) =>
-        React.render(<Home user={user} />, topNode))
+    return app.gitHub.user()
+      .then((user) => <Home user={user} />)
       .catch(errorHandler("loading user information"));
   }
+}
+
+$.get('config.json', (config) => {
+  app.errorBox = React.render(<ErrorBox />, document.querySelector('#errors'));
+
+  app.config = config;
+  route().then((view) =>
+    React.render(view, document.querySelector('#top'))
+  );
 });
