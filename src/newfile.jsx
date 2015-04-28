ef42b881fe8ed24c2c69bd5710a518a8a9772f69
Alex Morega <alex@grep.ro>
2015-04-28 16:48:07 +0300

    enter to create file

--- src/newfile.jsx
+++ src/newfile.jsx
@@ -3,9 +3,10 @@
 class NewFileModal extends React.Component {
   render() {
     return (
-      <div className="modal-content">
+      <form className="modal-content" onSubmit={this.handleSubmit.bind(this)}>
         <div className="modal-header">
-          <button className="close" data-dismiss="modal" aria-label="Close">
+          <button type="button" className="close"
+                  data-dismiss="modal" aria-label="Close">
             <span aria-hidden="true">&times;</span>
           </button>
           <h4 className="modal-title">New file</h4>
@@ -14,21 +15,22 @@ class NewFileModal extends React.Component {
           <input className="form-control" placeholder="path" ref="path" />
         </div>
         <div className="modal-footer">
-          <button className="btn btn-default" data-dismiss="modal">
+          <button type="button" className="btn btn-default"
+                  data-dismiss="modal">
             Cancel
           </button>
-          <button
-            className="btn btn-primary"
-            onClick={this.handleCreate.bind(this)}
-            >Create</button>
+          <button type="submit" className="btn btn-primary">
+            Create
+          </button>
         </div>
-      </div>
+      </form>
     );
   }
   componentDidMount() {
     setTimeout(() => React.findDOMNode(this.refs.path).focus(), 500);
   }
-  handleCreate() {
+  handleSubmit(evt) {
+    evt.preventDefault();
     var path = React.findDOMNode(this.refs.path).value.trim();
     app.hideModal();
     this.props.onCreate(path);
