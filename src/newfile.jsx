dcde7a900979100dd38013a1fb051ba6b23f6616
Alex Morega <alex@grep.ro>
2015-04-28 17:14:17 +0300

    validate file path

--- src/newfile.jsx
+++ src/newfile.jsx
@@ -1,7 +1,12 @@
 'use strict';
 
 class NewFileModal extends React.Component {
+  constructor (props) {
+    super(props);
+    this.state = {error: false, help: false};
+  }
   render() {
+    var mdUrl = 'https://help.github.com/articles/markdown-basics/';
     return (
       <form className="modal-content" onSubmit={this.handleSubmit.bind(this)}>
         <div className="modal-header">
@@ -12,7 +17,27 @@ class NewFileModal extends React.Component {
           <h4 className="modal-title">New file</h4>
         </div>
         <div className="modal-body">
-          <input className="form-control" placeholder="path" ref="path" />
+          <div className={'form-group' + (this.state.error ? ' has-error' : '' )}>
+            <input className="form-control" placeholder="path" ref="path" />
+            <span className="help-block">
+              Filename must end with <code>.md</code> or <code>.html</code> (
+              <a className="buttonlink"
+                  onClick={() => this.setState({help: true})}
+                  >why?</a>)
+            </span>
+          </div>
+          {this.state.help ?
+            <div>
+              <p>
+                <code>.md</code> is for <a href={mdUrl}
+                target="_blank">Markdown</a>, a powerful wiki-like format that
+                gets compiled to HTML. You can edit the source code and get a
+                preview underneath. <code>.html</code> is for plain HTML files.
+                You can edit the content in a friendly visual editor. Choose
+                whichever you like.
+              </p>
+            </div>
+          : null}
         </div>
         <div className="modal-footer">
           <button type="button" className="btn btn-default"
@@ -32,7 +57,12 @@ class NewFileModal extends React.Component {
   handleSubmit(evt) {
     evt.preventDefault();
     var path = React.findDOMNode(this.refs.path).value.trim();
-    app.hideModal();
-    this.props.onCreate(path);
+    if(path.match(/\.(md|markdown|html)$/)) {
+      app.hideModal();
+      this.props.onCreate(path);
+    }
+    else {
+      this.setState({error: true});
+    }
   }
 }
