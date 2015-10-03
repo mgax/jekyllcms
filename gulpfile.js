0261ad3751aa03305e70c5bca755672efc376189
Alex Morega <alex@grep.ro>
2015-10-03 09:39:15 +0300

    remove semicolons from gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,9 @@
-var fs = require('fs');
-var gulp = require('gulp');
-var babel = require('gulp-babel');
-var concat = require('gulp-concat');
-var sourcemaps = require('gulp-sourcemaps');
-var Handlebars = require('handlebars');
+var fs = require('fs')
+var gulp = require('gulp')
+var babel = require('gulp-babel')
+var concat = require('gulp-concat')
+var sourcemaps = require('gulp-sourcemaps')
+var Handlebars = require('handlebars')
 
 gulp.task('js', function() {
   return gulp.src('src/*.jsx')
@@ -11,20 +11,20 @@ gulp.task('js', function() {
     .pipe(babel())
     .pipe(concat('app.js'))
     .pipe(sourcemaps.write('./'))
-    .pipe(gulp.dest('build'));
-});
+    .pipe(gulp.dest('build'))
+})
 
 gulp.task('build', ['js'], function() {
   var template = function(name) {
-    return Handlebars.compile(fs.readFileSync(name, {encoding: 'utf-8'}));
-  };
-  var index_html = template('src/index.html')({t: (new Date()).getTime()});
-  fs.writeFileSync('build/index.html', index_html);
-  fs.writeFileSync('build/style.css', fs.readFileSync('src/style.css'));
-});
+    return Handlebars.compile(fs.readFileSync(name, {encoding: 'utf-8'}))
+  }
+  var index_html = template('src/index.html')({t: (new Date()).getTime()})
+  fs.writeFileSync('build/index.html', index_html)
+  fs.writeFileSync('build/style.css', fs.readFileSync('src/style.css'))
+})
 
 gulp.task('devel', ['build'], function() {
-  gulp.watch('src/*.jsx', ['build']);
-});
+  gulp.watch('src/*.jsx', ['build'])
+})
 
-gulp.task('default', ['build']);
+gulp.task('default', ['build'])
